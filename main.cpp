a572ea356b23bfbeee5fea90deb6cbbfdbbf9a91
tuanhd <duytuan@bluedragonvn.com>
2025-08-24 20:25:56 +0700

    - Feat:  Add init funtion to main.

--- main.cpp
+++ main.cpp
@@ -3,3 +3,23 @@
 #include <cstdlib>
 #include <conio.h>
 using namespace std;
+
+struct Point{
+    int x,y;
+};
+class CONRAN {
+public:
+    struct Point A[100];
+    int DoDai;
+
+    CONRAN() {
+        init();
+    }
+
+    void init() {
+        DoDai = 3;
+        A[0] = {10, 10};
+        A[1] = {11, 10};
+        A[2] = {12, 10};
+    }
+};
