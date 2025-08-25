6ac6a7d84c7ab3e165df602e8a38d59c3b3e34ca
24730227-Le-Trong-Quy <lequy00369@gmail.com>
2025-08-25 15:07:55 +0700

    [Add] thêm endline

--- main.cpp
+++ main.cpp
@@ -5,14 +5,17 @@
 using namespace std;
 
 
-void Draw (SNAKE snake){
+void Draw (SNAKE snake)
+{
     int i;
-    for (i=0;i<snake.Leng;i++){
+    for (i = 0; i < snake.Leng; i++)
+    {
         gotoxy(snake.A[i].x*2, snake.A[i].y);
-        printf("%c",219);
+        printf("%c%c",219,219);
         Sleep(1000);
     }
     gotoxy(snake.A[i].x*2, snake.A[i].y);
     printf(" ");
     gotoxy(40,20);
+    printf("\n");
 }
