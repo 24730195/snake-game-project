6486dbf1e8d9a151b9f5a814a43b92085d3dab4f
24730195 <24730195@ms.uit.edu.vn>
2025-08-25 19:58:56 +0700

    Create function DrawFrame

--- main.cpp
+++ main.cpp
@@ -4,10 +4,45 @@
 #include <conio.h>
 using namespace std;
 
+// Kích thước khung
+const int MIN_WIDTH = 10;
+const int MAX_WIDTH = 95;
+const int MIN_HEIGHT = 1;
+const int MAX_HEIGHT = 25;
+
+// Khai báo hàm
+void gotoxy(int x, int y);
+void DrawFrame();
+
+int main() {
+    // Dựng trò chơi
+    // ---Cài đặt---
+    DrawFrame();
+
+    return 0;
+}
+
 // Hàm di chuyển con trỏ in ra màn hình console
 void gotoxy(int x, int y) {
     COORD coord;
     coord.X = x;
     coord.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+// Hàm vẽ khung
+void DrawFrame() {
+    for (int x = MIN_WIDTH; x <= MAX_WIDTH; x++) {
+        gotoxy(x, MIN_HEIGHT);
+        cout << "#";   // cạnh trên
+        gotoxy(x, MAX_HEIGHT);
+        cout << "#";   // cạnh dưới 
+    }
+    
+    for (int y = MIN_HEIGHT; y <= MAX_HEIGHT; y++) {
+        gotoxy(MIN_WIDTH, y);
+        cout << "#";   // cạnh trái
+        gotoxy(MAX_WIDTH, y);
+        cout << "#";   // cạnh phải
+    }
 }
