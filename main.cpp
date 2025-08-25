3112ae1f6bde10126584549b43a2ffa6d883f534
24730227-Le-Trong-Quy <lequy00369@gmail.com>
2025-08-25 16:59:03 +0700

    [Add] Tạo main control

--- main.cpp
+++ main.cpp
@@ -3,3 +3,30 @@
 #include <cstdlib>
 #include <conio.h>
 using namespace std;
+
+
+
+int main()
+{
+    CONRAN r;
+    int Huong = 0;
+    char t;
+
+    while (1)
+    {
+        if (kbhit())
+        {
+            t = getch();
+            if (t=='a') Huong = 2;
+            if (t=='w') Huong = 3;
+            if (t=='d') Huong = 0;
+            if (t=='x') Huong = 1;
+        }
+        system("cls");
+        r.Draw();
+        r.DiChuyen(Huong);
+        Sleep(1000);
+    }
+
+    return 0;
+}
