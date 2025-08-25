2454773e23a972b54a681738a050fb1410d753c6
24730247.Trương Hoàng Thanh Vy <24730247@ms.uit.edu.vn>
2025-08-25 18:45:44 +0700

    [Update] main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-const int WIDTH = 40;   // chiều rộng khung
-const int HEIGHT = 20;  // chiều cao khung
-
 // Hàm di chuyển con trỏ in ra màn hình console
 void gotoxy(int x, int y) {
     COORD coord;
@@ -9,19 +6,8 @@ void gotoxy(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-// Hàm vẽ khung trò chơi
-void DrawFrame() {
-    for (int x = 0; x <= WIDTH; x++) {
-        gotoxy(x, 0);      cout << "#";   // cạnh trên
-        gotoxy(x, HEIGHT); cout << "#";   // cạnh dưới
-    }
-    for (int y = 0; y <= HEIGHT; y++) {
-        gotoxy(0, y);      cout << "#";   // cạnh trái
-        gotoxy(WIDTH, y);  cout << "#";   // cạnh phải
-    }
-}
-
 int main() {
-  DrawFrame();
+    gotoxy(10, 5);
+    printf("Hello");
     return 0;
 }
