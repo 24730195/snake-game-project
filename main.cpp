a0ff3cdec79a2b6d0121ef3a26e946429676e1e1
24730247.Trương Hoàng Thanh Vy <24730247@ms.uit.edu.vn>
2025-08-25 18:20:48 +0700

    [Update] function gotoxy

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
-#include <iostream>
-#include <windows.h>
-#include <cstdlib>
-#include <conio.h>
-using namespace std;
+const int WIDTH = 40;   // chiều rộng khung
+const int HEIGHT = 20;  // chiều cao khung
+
+// Hàm di chuyển con trỏ in ra màn hình console
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+// Hàm vẽ khung trò chơi
+void DrawFrame() {
+    for (int x = 0; x <= WIDTH; x++) {
+        gotoxy(x, 0);      cout << "#";   // cạnh trên
+        gotoxy(x, HEIGHT); cout << "#";   // cạnh dưới
+    }
+    for (int y = 0; y <= HEIGHT; y++) {
+        gotoxy(0, y);      cout << "#";   // cạnh trái
+        gotoxy(WIDTH, y);  cout << "#";   // cạnh phải
+    }
+}
+
+int main() {
+  DrawFrame();
+        snake.Draw();
+}
+
+    return 0;
+}
