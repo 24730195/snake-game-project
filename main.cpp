0c13e9431e0b3bd44430890b51d0b6c6f137ecf9
24730247.Trương Hoàng Thanh Vy <24730247@ms.uit.edu.vn>
2025-08-25 19:47:55 +0700

    refactor: remove test main function, keep only gotoxy()

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+#include <conio.h>
+using namespace std;
+
 // Hàm di chuyển con trỏ in ra màn hình console
 void gotoxy(int x, int y) {
     COORD coord;
