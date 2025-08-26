4eb34adbbfbfd875e6df1e142f54f984f36bf00a
24730227-Le-Trong-Quy <lequy00369@gmail.com>
2025-08-26 17:39:07 +0700

    [merge] create-function-draw-Quy to create-function-generatefood-vy

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ const int MAX_HEIGHT = 25;
 // Khai báo hàm
 void gotoxy(int x, int y);
 void DrawFrame();
+Point food;
 
 //Khởi tạo class Snake
 class Snake {
@@ -62,6 +63,7 @@ int main() {
     return 0;
 }
 
+
 // Hàm di chuyển con trỏ in ra màn hình console
 void gotoxy(int x, int y) {
     COORD coord;
@@ -87,3 +89,23 @@ void DrawFrame() {
         cout << "#";   // cạnh phải
     }
 }
+// Hàm sinh mồi ngẫu nhiên
+void generateFood() {
+    food.x = rand() % (MAX_WIDTH - MIN_WIDTH - 1) + MIN_WIDTH + 1;
+    food.y = rand() % (MAX_HEIGHT - MIN_HEIGHT - 1) + MIN_HEIGHT + 1;
+}
+
+// Hàm kiểm tra rắn ăn mồi
+void eatFood(Snake &snake) {
+    if (snake.Body[0].x == food.x && snake.Body[0].y == food.y) {
+        snake.Length++;
+        generateFood();
+    }
+}
+
+// Hàm vẽ mồi
+void drawFood() {
+    gotoxy(food.x, food.y);
+    cout << "o";
+}
+
